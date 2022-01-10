eb26ae120568373c315150c1be9f5f272857fad0
Srinivas Reddy <solipuramsrinivas@gmail.com>
2022-01-10 21:47:08 +0530

    changes made on Jenkinsfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM python:3-alpine
+FROM python:slim-buster
 ADD hello_world.py /home/hello_world.py
 CMD ["python","-u","hello_world.py"]
--- libraries/hello/steps/helloMethod.groovy
+++ libraries/hello/steps/helloMethod.groovy
@@ -4,8 +4,8 @@ void call()
 	{
 		println"Welcome to JTE:Demo"
 		println"We are builing a docker Container"
-		println"Push  a Container to Docker Hub"
-		println"Pull  a Container from Docker Hub"
+		println"Push a Container to Docker Hub"
+		println"Pull a Container from Docker Hub"
 	
 	}
 
