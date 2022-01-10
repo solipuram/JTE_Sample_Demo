8bee6bd04e8bf01880daf26bfd8ed628ac69c318
Srinivas Reddy <solipuramsrinivas@gmail.com>
2022-01-10 09:14:43 +0530

    adding git stage

--- libraries/git/steps/checkout.groovy
+++ libraries/git/steps/checkout.groovy
@@ -3,6 +3,7 @@ void call
     stage("Git:Checkout")
     {
 
+       
         echo " Working on Git Stage"
         //git url: "${config.url}", credentialsId: "${config.crednetialsId}", branch: "${config.branch}"
        
