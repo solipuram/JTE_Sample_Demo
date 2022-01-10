8a5c48a63b5c2965425c0483b7f44a7b1c291566
Srinivas Reddy <solipuramsrinivas@gmail.com>
2022-01-10 08:09:08 +0530

    adding git

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,10 +1,5 @@
 libraries{
     hello
-    git{
-             url = "https://github.com/solipuram/JTE_Sample_Demo.git"
-            credentialsId = "ci_workflow_demo"
-            branch = "dev"
-
-    }
+    git
         
 }
