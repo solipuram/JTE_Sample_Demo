70a4631b6615e03d41ef1720b38bbe5a304dbaa5
Srinivas Reddy <solipuramsrinivas@gmail.com>
2022-01-10 19:33:30 +0530

    Adding Docker Push Stage

--- libraries/docker/steps/buildImage.groovy
+++ libraries/docker/steps/buildImage.groovy
@@ -32,8 +32,8 @@ void docker_image_tagging()
 
 void pushImage()
 {
-    echo "docker login ---username= ${config.login.DOCKER_USERNAME} --password= ${config.login.DOCKER_PASSWORD} "
-    sh "docker login --username = ${config.login.DOCKER_USERNAME} --password=${config.login.DOCKER_PASSWORD}"   
+    echo "docker login ---username ${config.login.DOCKER_USERNAME} --password ${config.login.DOCKER_PASSWORD} "
+    sh "docker login --username  ${config.login.DOCKER_USERNAME} --password ${config.login.DOCKER_PASSWORD}"   
     echo "docker push ${config.registry.url}/${config.registry.repository}/${config.image.name}:latest"
     sh  "docker push ${config.registry.url}/${config.registry.repository}/${config.image.name}:latest"
  
