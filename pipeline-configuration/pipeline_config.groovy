eb8527566f2d3f69db2d1419299b7ec96d75523f
Srinivas Reddy <solipuramsrinivas@gmail.com>
2022-01-10 19:42:30 +0530

    Adding Docker Push Stage

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -13,7 +13,7 @@ libraries{
           }
           login{
               DOCKER_USERNAME= "solipuramsrinivas"
-              DOCKER_PASSWORD="Dhanu@9968"
+              DOCKER_PASSWORD="239be91c-d39c-49f8-b1c4-039a3e1a39a2"
           }
           registry{
               url="solipuramsrinivas"
